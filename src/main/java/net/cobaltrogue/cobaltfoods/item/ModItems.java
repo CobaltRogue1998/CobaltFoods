d8ac58e6af51c913cb4cd7a5e8de37747a636220
CobaltRogue1998 <cobaltrogue98@outlook.com>
2025-04-02 19:49:31 -0400

    Add toast barely

---
+++ src/main/java/net/cobaltrogue/cobaltfoods/item/ModItems.java
@@ -0,0 +1,20 @@
+package net.cobaltrogue.cobaltfoods.item;
+
+import net.cobaltrogue.cobaltfoods.CobaltFoods;
+import net.minecraft.world.food.Foods;
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class ModItems {
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(CobaltFoods.MOD_ID);
+
+    public static final DeferredItem<Item> TOAST = ITEMS.register("toast",
+            () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus){
+        ITEMS.register(eventBus);
+    }
+}
+
